7421639cfedaba4441e982873956f98eb48d012d
Masatoshi Yoshida <pooneil68@gmail.com>
2022-11-11 12:04:45 +0900

    Delete codes directory

--- codes/computeCliff_delta.m
+++
@@ -1,8 +0,0 @@
-function Cliff_d = computeCliff_delta(sal_sz, sal_hc, stats)
-% Yoshida 20220429
-  
-    W = stats.ranksum;
-    U = W - length(sal_sz)*(length(sal_sz)+1)/2;
-    Cliff_d = 2 * U / (length(sal_sz) * length(sal_hc)) - 1;
-
-end
--- codes/subject_label12.m
+++
@@ -1,8 +0,0 @@
-clearvars;
-data = readtable('subject_label12.csv', 'FileType','text',...
-                         'ReadVariableNames', 0, 'Delimiter', ',');
-sc = [];
-% diag	age
-sc.demographics = [data.Var1 data.Var2];
-
-save('subject_label12.mat', 'sc');
